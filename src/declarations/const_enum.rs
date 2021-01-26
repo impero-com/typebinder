e7b0af96c3f09a734ee20a37c72c62c1ac315a35
Olivier Pinon <op@impero.com>
2021-01-26 19:37:32 +0100

    Test coverage for TypeAlias & ConstEnum

--- src/declarations/const_enum.rs
+++ src/declarations/const_enum.rs
@@ -1,4 +1,5 @@
 use crate::common::StringLiteral;
+use askama::Template;
 
 /*
 ConstEnumDeclaration
@@ -12,19 +13,82 @@ ConstEnumVariantList
     Identifier = StringLiteralValue , ConstEnumVariantList
 */
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Template)]
+#[template(source = "const enum {{ ident }} {{ body }}", ext = "txt")]
 pub struct ConstEnumDeclaration {
     pub ident: String,
     pub body: ConstEnumBody,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Template)]
+#[template(source = "{ {{ variants|join(\", \") }} }", ext = "txt")]
 pub struct ConstEnumBody {
-    pub variants: ConstEnumVariant,
+    pub variants: Vec<ConstEnumVariant>,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Template)]
+#[template(source = "{{ ident }} = {{ value }}", ext = "txt")]
 pub struct ConstEnumVariant {
-    pub identifier: String,
+    pub ident: String,
     pub value: StringLiteral,
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn display_const_enum_declaration() {
+        assert_eq!(
+            ConstEnumDeclaration {
+                ident: "MyEnum".to_string(),
+                body: ConstEnumBody {
+                    variants: vec![
+                        ConstEnumVariant {
+                            ident: "One".to_string(),
+                            value: StringLiteral::from("one"),
+                        },
+                        ConstEnumVariant {
+                            ident: "Two".to_string(),
+                            value: StringLiteral::from("two"),
+                        },
+                    ]
+                }
+            }
+            .to_string(),
+            "const enum MyEnum { One = \"one\", Two = \"two\" }",
+        );
+    }
+
+    #[test]
+    fn display_const_enum_body() {
+        assert_eq!(
+            ConstEnumBody {
+                variants: vec![
+                    ConstEnumVariant {
+                        ident: "One".to_string(),
+                        value: StringLiteral::from("one"),
+                    },
+                    ConstEnumVariant {
+                        ident: "Two".to_string(),
+                        value: StringLiteral::from("two"),
+                    },
+                ]
+            }
+            .to_string(),
+            "{ One = \"one\", Two = \"two\" }"
+        )
+    }
+
+    #[test]
+    fn display_const_enum_variant() {
+        assert_eq!(
+            ConstEnumVariant {
+                ident: "MyVariant".to_string(),
+                value: StringLiteral::from("TheValue"),
+            }
+            .to_string(),
+            "MyVariant = \"TheValue\"",
+        );
+    }
 }
--- src/declarations/type_alias.rs
+++ src/declarations/type_alias.rs
@@ -1,4 +1,6 @@
+use crate::common::filters;
 use crate::types::{TsType, TypeParameters};
+use askama::Template;
 
 /*
 TypeAlias
@@ -7,9 +9,33 @@ TypeAliasDeclaration
     type Identifier TypeParametersopt = Type ;
 */
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Template)]
+#[template(
+    source = "type {{ ident }} {{- params|display_opt }} = {{ inner_type }};",
+    ext = "txt"
+)]
 pub struct TypeAliasDeclaration {
     pub ident: String,
     pub params: Option<TypeParameters>,
     pub inner_type: TsType,
+}
+
+#[cfg(test)]
+pub mod tests {
+    use crate::types::{PredefinedType, PrimaryType};
+
+    use super::*;
+
+    #[test]
+    fn display_type_alias_declaration() {
+        assert_eq!(
+            TypeAliasDeclaration {
+                ident: "MyType".to_string(),
+                params: None,
+                inner_type: TsType::PrimaryType(PrimaryType::Predefined(PredefinedType::Any)),
+            }
+            .to_string(),
+            "type MyType = any;",
+        )
+    }
 }
