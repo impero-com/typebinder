e77308c8921ead1571aa9b533894bbce5440d1de
Olivier Pinon <op@impero.com>
2021-04-08 10:02:51 +0200

    Better strong-types

--- ts_json_subset/src/common.rs
+++ ts_json_subset/src/common.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+
 use displaythis::Display;
 
 /// Askama filters
@@ -15,19 +17,20 @@ pub mod filters {
 /// An escaped string literal, surrounded by double quotes.
 pub struct StringLiteral(String);
 
-// TODO: Remove the string copy ?
-// TODO: Check that input is escaped
-impl<'a> From<&'a str> for StringLiteral {
-    fn from(input: &str) -> Self {
-        StringLiteral(input.to_string())
+impl StringLiteral {
+    pub fn from_raw(input: &str) -> Self {
+        StringLiteral::from(input.to_string())
     }
 }
 
-// TODO: Check that input is escaped
 impl From<String> for StringLiteral {
     fn from(input: String) -> Self {
-        StringLiteral(input)
+        StringLiteral(sanitize_string_literal(&input))
     }
+}
+
+pub fn sanitize_string_literal(input: &str) -> String {
+    input.escape_default().collect()
 }
 
 #[derive(Debug, Clone, PartialEq, Display)]
@@ -35,10 +38,18 @@ impl From<String> for StringLiteral {
 /// A numeric literal, must have a numeric value (ie. no Infinity, no NaN)
 pub struct NumericLiteral(f64);
 
-// TODO: Check that input is a numeric value, use f64::is_finite
-impl From<f64> for NumericLiteral {
-    fn from(input: f64) -> Self {
-        NumericLiteral(input)
+#[derive(Debug, Clone, PartialEq, Display)]
+#[display("{0} is not a valid numeric literal")]
+pub struct WrongNumericLiteral(f64);
+
+impl TryFrom<f64> for NumericLiteral {
+    type Error = WrongNumericLiteral;
+
+    fn try_from(input: f64) -> Result<Self, Self::Error> {
+        if input.is_finite() {
+            return Ok(NumericLiteral(input));
+        }
+        return Err(WrongNumericLiteral(input));
     }
 }
 
@@ -50,5 +61,31 @@ pub struct BooleanLiteral(bool);
 impl From<bool> for BooleanLiteral {
     fn from(input: bool) -> Self {
         BooleanLiteral(input)
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    pub fn should_escape_string_literal() {
+        assert_eq!(
+            StringLiteral::from_raw("my \"string\" literal"),
+            StringLiteral("my \\\"string\\\" literal".to_string()),
+        );
+    }
+
+    #[test]
+    pub fn should_validate_numeric_literal() {
+        assert_eq!(NumericLiteral::try_from(1.2), Ok(NumericLiteral(1.2)),);
+        assert!(matches!(
+            NumericLiteral::try_from(f64::INFINITY),
+            Err(WrongNumericLiteral(_)),
+        ));
+        assert!(matches!(
+            NumericLiteral::try_from(f64::NAN),
+            Err(WrongNumericLiteral(_)),
+        ));
     }
 }
