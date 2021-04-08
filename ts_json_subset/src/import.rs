412137ae3704edd87410c5c67a96e195af6cbe1d
Olivier Pinon <op@impero.com>
2021-04-08 13:52:53 +0200

    More usage of TSIdent

--- ts_json_subset/src/import.rs
+++ ts_json_subset/src/import.rs
@@ -1,6 +1,8 @@
 use askama::Template;
 use displaythis::Display;
 
+use crate::ident::TSIdent;
+
 #[derive(Debug, Clone, PartialEq, Template)]
 #[template(source = "import {{ import_kind }} from {{ path }};", ext = "txt")]
 /// An import statement, supporting multiple imports from a file
@@ -14,19 +16,16 @@ pub struct ImportStatement {
 #[template(source = "{{ items|join(\", \") }}", ext = "txt")]
 /// An list of imported identifiers
 pub struct ImportList {
-    // TODO: Make an identifier type that checks TS constraints on identifiers
-    pub items: Vec<String>,
+    pub items: Vec<TSIdent>,
 }
 
 #[derive(Debug, Clone, PartialEq, Display)]
 /// The identifiers fragment of an import statement
 pub enum ImportKind {
     #[display("{0}")]
-    // TODO: Make an identifier type that checks TS constraints on identifiers
-    Identifier(String),
+    Identifier(TSIdent),
     #[display("* as {0}")]
-    // TODO: Make an identifier type that checks TS constraints on identifiers
-    GlobAsIdentifier(String),
+    GlobAsIdentifier(TSIdent),
     #[display("{{ {0} }}")]
     // TODO: Inline ImportList ?
     ImportList(ImportList),
@@ -34,13 +33,18 @@ pub enum ImportKind {
 
 #[cfg(test)]
 pub mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
     fn display_import_list() {
         assert_eq!(
             ImportList {
-                items: vec!["Test".to_string(), "TestOther".to_string(),],
+                items: vec![
+                    TSIdent::from_str("Test").unwrap(),
+                    TSIdent::from_str("TestOther").unwrap()
+                ],
             }
             .to_string(),
             "Test, TestOther",
@@ -51,7 +55,7 @@ pub mod tests {
     fn display_import_statement() {
         assert_eq!(
             ImportStatement {
-                import_kind: ImportKind::Identifier("Test".to_string()),
+                import_kind: ImportKind::Identifier(TSIdent::from_str("Test").unwrap()),
                 path: r#""types/users""#.to_string(),
             }
             .to_string(),
@@ -60,7 +64,7 @@ pub mod tests {
 
         assert_eq!(
             ImportStatement {
-                import_kind: ImportKind::GlobAsIdentifier("Test".to_string()),
+                import_kind: ImportKind::GlobAsIdentifier(TSIdent::from_str("Test").unwrap()),
                 path: r#""types/users""#.to_string(),
             }
             .to_string(),
@@ -70,7 +74,10 @@ pub mod tests {
         assert_eq!(
             ImportStatement {
                 import_kind: ImportKind::ImportList(ImportList {
-                    items: vec!["Test".to_string(), "TestOther".to_string()]
+                    items: vec![
+                        TSIdent::from_str("Test").unwrap(),
+                        TSIdent::from_str("TestOther").unwrap()
+                    ]
                 }),
                 path: r#""types/users""#.to_string(),
             }
